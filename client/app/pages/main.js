782e65cea74b5267fad926d6d8d6881923250f55
Robin Gulbrandsen <robin@123.no>
2016-12-05 16:50:10 -0800

    Add nav bar

--- client/app/pages/main.js
+++ client/app/pages/main.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar,
+         Nav,
+         NavItem } from 'react-bootstrap';
 
 export default class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1>Main Page!</h1>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">RobinGulbrandsen</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <LinkContainer to="home">
+                <NavItem eventKey={1}>Home</NavItem>
+              </LinkContainer>
+              <LinkContainer to="about">
+                <NavItem eventKey={1}>About</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         {this.props.children}
       </div>
     );
