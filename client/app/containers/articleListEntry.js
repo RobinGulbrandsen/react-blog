af2f310b4cb18722807f14a8807c1716b59c79aa
Robin Gulbrandsen <robin@123.no>
2016-12-06 19:07:39 -0800

    Format Date

--- client/app/containers/articleListEntry.js
+++ client/app/containers/articleListEntry.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router';
 import {connect} from 'react-redux';
+import dateFormat from '../util/timeFormat';
 
 import { Row } from 'react-bootstrap';
 
 class ArticleListEntry extends React.Component {
+
+  constructor(props) {
+    super(props);
+    
+    console.log('hello');
+    console.log(this.props.article);
+    this.time = dateFormat(this.props.article.createdAt);
+  }
+
   render() {
-    return (<div className='article'>
-      <Row>
-        <h2>
-          {this.props.article.title}
-        </h2>
-        <p>
-          <span className="glyphicon glyphicon-time"> </span>
-          {this.props.article.createdAt}
-        </p>
-        <p className="bold">
-          {this.props.article.intro}
-        </p>
-        <Link to={'article/' + this.props.article.id}>Read More</Link>
-        <hr />
-      </Row>
-    </div>);
+    return (
+      <div className='article'>
+        <Row>
+          <h2>
+            {this.props.article.title}
+          </h2>
+          <p>
+            <span className="glyphicon glyphicon-time"></span>
+            <span> {this.time}</span>
+          </p>
+          <p className="bold">
+            {this.props.article.intro}
+          </p>
+          <Link to={'article/' + this.props.article.id}>Read More</Link>
+          <hr />
+        </Row>
+      </div>
+    );
   }
 }
 
---
+++ client/app/util/timeFormat.js
@@ -0,0 +1,22 @@
+export default (date) => {
+  console.log('formatting', date);
+
+  const months = ["January", "February", "March", "April", "May", "June", 
+  "July", "August", "September", "October", "November", "December"];
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  const getOrdinal = (n) => {
+    const s = ["th","st","nd","rd"];
+    const v = n % 100;
+    return n+(s[(v-20)%10]||s[v]||s[0]);
+  };
+
+  date = date.split('T')[0] || '2016-01-01';
+  date = date.split('-');
+
+  const year = Number(date[0]);
+  const month = Number(date[1]);
+  const day = Number(date[2]);
+
+  return months[month - 1] + ' ' + getOrdinal(day) + ' ' + year;
+};
