bd635ab97b38495f8df6e1e270e010330fca131b
Robin Gulbrandsen <robin@123.no>
2016-12-19 06:58:26 +0100

    update location to undefined

--- client/app/containers/aboutMeSmall.js
+++ client/app/containers/aboutMeSmall.js
@@ -8,7 +8,7 @@ const AboutMeSmall = () => (
     <Well>
       <h3>About the Author</h3>
       <p>
-        Java and web developer located in San Francisco, CA with passion for software and innovative solutions. 
+        Java and web developer with passion for software and innovative solutions. 
         This blog is a playground for testing technology, sharing thoughts and saving concepts
       </p>
       <Link to='about'>Read More</Link>
