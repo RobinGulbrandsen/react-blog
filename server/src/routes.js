9082d6019b7c249a9089c4df3e01c18e8cb67b31
Robin Gulbrandsen <robin@123.no>
2016-12-19 07:54:55 +0100

    allign indentations

--- server/src/routes.js
+++ server/src/routes.js
@@ -7,18 +7,18 @@ import passport from 'passport';
 module.exports = (app) => {
 
   //////// ARTICLES ////////////
-  app.get('/api/articles',          ArticleController.readAll);
-  app.get('/api/articles/:id',      ArticleController.read);
-  app.post('/api/articles',         ArticleController.create);
-  app.put('/api/articles',          ArticleController.update);
-  app.delete('/api/articles/:id',   ArticleController.destroy);
+  app.get('/api/articles',        ArticleController.readAll);
+  app.get('/api/articles/:id',    ArticleController.read);
+  app.post('/api/articles',       ArticleController.create);
+  app.put('/api/articles',        ArticleController.update);
+  app.delete('/api/articles/:id', ArticleController.destroy);
 
   //////// ARTICLES ////////////
-  app.get('/api/projects',          ProjectsController.readAll);
+  app.get('/api/projects',        ProjectsController.readAll);
 
   //////// USERS ////////////
-  app.post('/api/logout',           UserController.logout);
-  app.post('/api/signup',           UserController.signup);
+  app.post('/api/logout',         UserController.logout);
+  app.post('/api/signup',         UserController.signup);
   app.post('/api/login', passport.authenticate('local', { successRedirect: '/#/admin',
                                                           failureRedirect: '/#/login' }));
 };
